90eaf3154054665b017f848604d7aa6a21c93580
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-07-30 22:00:01 +0530

    Update files

--- basic.py
+++ basic.py
@@ -1,6 +1,19 @@
-a,b=list(map(int,input().split()))
-a,b=b,a
-print(a,b)
-# swapping 2 no 
+# a,b=list(map(int,input().split()))
+# a,b=b,a
+# print(a,b)
+# # swapping 2 no 
 
 
+#3014
+def minimumpushes(word):
+    n= len(word)
+    ans =0
+    i=1
+    while n>0:
+        ans+=i*min(8,n)
+        n-=8
+        i+=1
+    return ans
+
+
+print(minimumpushes("helloworld"))
