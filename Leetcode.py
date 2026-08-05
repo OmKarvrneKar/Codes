bb7c10082b25107f0493e3c3b1452c1dfabb3acb
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-05 21:45:38 +0530

    Implement depth-first search algorithm and add example graph

--- Leetcode.py
+++ Leetcode.py
@@ -74,22 +74,52 @@ def deletenode(head, data):
 
 
 # merge two sorted linked listk
-# class Solution(object):
-#     def mergeTwoLists(self, list1, list2):
-#         dummy = ListNode()
-#         tail = dummy
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
 
-#         while list1 and list2:
-#             if list1.val < list2.val:
-#                 tail.next = list1
-#                 list1 = list1.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
 
-#             else:
-#                 tail.next = list2
-#                 list2 = list2.next
-#             tail = tail.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
 
-#         tail.next = list1 if list1 else list2
+        tail.next = list1 if list1 else list2
 
-#         return dummy.next
+        return dummy.next
+        
+#  110. Balanced Binary Tree
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def dfs(node):
+            if not node:
+                return 0
+            
+            # 1. Check left subtree balance and height
+            left_height = dfs(node.left)
+            if left_height == -1:
+                return -1
+                
+            
+            right_height = dfs(node.right)
+            if right_height == -1:
+                return -1
+            
+            # 3. Check current node balance
+            if abs(left_height - right_height) > 1:
+                return -1
+                
+            # 4. Return actual height to parent node
+            return 1 + max(left_height, right_height)
+            
+        return dfs(root) != -1
         
