49795794ceafef0bbe477689c5b60c39f1cbca52
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-06 19:44:24 +0530

    Add grade and case examples

--- basic.py
+++ basic.py
@@ -181,3 +181,20 @@ if m>0:
     print("positive")
 else: print("negative")
 #=========================
+marks=int(input("enter the marks"))
+if marks>=90:
+    print("A")
+elif marks>=80 and marks<90:
+    print("B")
+elif marks>=70 and marks<80:
+    print("C")  
+elif marks>=60 and marks<70:
+    print("D")
+else :
+    print("F")
+#======================
+char1= input("enter the letter")
+if char1.isupper():
+    print("uppercase")
+else:
+    print("lowercase")
