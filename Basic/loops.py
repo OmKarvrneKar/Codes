89d3b68bb22744ca2a3929cd05e3068ed3fa2b0d
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-07 19:56:41 +0530

    Update Basic/loops.py examples

--- Basic/loops.py
+++ Basic/loops.py
@@ -40,44 +40,35 @@
 #     print("*" * i)
 
 #==========================
-# n=12345
-# print("Reverse of the number is: 12345 ",str(n)[::-1])
+n=12345
+print("Reverse of the number is: 12345 ",str(n)[::-1])
 # #========================
 n=987654
-# print("count of digits in the number is: 987654 ",len(str(n)))
+print("count of digits in the number is: 987654 ",len(str(n)))
 # #========================
-# n=12345
-# sum=0
-# for i in str(n):
-#     sum+=int(i)
-# print("sum of digits in the number is: 12345 ",sum)
+n=12345
+sum=0
+for i in str(n):
+    sum+=int(i)
+print("sum of digits in the number is: 12345 ",sum)
 # #========================
-# n=121
-# if str(n)==str(n)[::-1]:
-#     print("The number is palindrome")
-# else:
-#     print("The number is not palindrome")
+n=121
+if str(n)==str(n)[::-1]:
+    print("The number is palindrome")
+else:
+    print("The number is not palindrome")
 # #========================
-# n=153
-# sum=0
-# for i in str(n):
-#     sum+=int(i)**len(str(n))
-#     if sum==n:
-#         print("The number is armstrong" )
-#     else:
-#         print("The number is not armstrong")
+n=153
+sum=0
+for i in str(n):
+    sum+=int(i)**len(str(n))
+    if sum==n:
+        print("The number is armstrong" )
+    else:
+        print("The number is not armstrong")
 
 #=========================
-# n=18
-# for i in range(2,n):
-#     if n%i==0:
-#         print("The number is not prime")
-#         break
-#     else:
-#         print("The number is prime")
-#         break
-#=========================
-n=100
+n=18
 for i in range(2,n):
     if n%i==0:
         print("The number is not prime")
@@ -85,4 +76,13 @@ for i in range(2,n):
     else:
         print("The number is prime")
         break
+#=========================
+def fubo(n):
+    a,b=0,1
+    result=[]
+    for i in range(n):
+        result.append(a)
+        a,b=b,a+b
+    return result
+print(fubo(10))
 
