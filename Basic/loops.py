fdde3a821286aa9f0999234d2f04638238c3aec5
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-07 19:26:33 +0530

    Update loops examples

--- Basic/loops.py
+++ Basic/loops.py
@@ -1,9 +1,47 @@
-for i in range(1,11):
-    print(i)
+# for i in range(1,11):
+#     print(i)
 
-#=========================
-for j in range(10,0,-1):
-    print(j)
-#=========================
-for k in range(2,21,2):
-    print(k)
+# # #=========================
+# for j in range(10,0,-1):
+#     print(j)
+# # #=========================
+# for k in range(2,21,2):
+#     print(k)
+# # #=========================
+# for l in range(1,20,2):
+#     print(l)
+# #=========================S
+# for m in range(7,71,7):
+#     print(m)
+# #==========================
+
+# sum=0
+# for i in range(1,101):
+#     sum+=i
+# print(sum)
+
+# #========================
+
+# sum=0
+# for i in range(2,101,2):
+#     sum+=i
+# print(sum)
+# #========================
+# Input=int(input(" "))
+# fact=1
+# for i in range(1,Input+1):
+#     fact*=i
+# print(fact)
+# #====================
+# for  i in range(50,101):
+#     print(i)
+# #=========================
+# for  i in range(1,6):
+#     print("*" * i)
+
+#==========================
+# n=12345
+# print("Reverse of the number is: 12345 ",str(n)[::-1])
+# #========================
+n=987654
+print("count of digits in the number is: 987654 ",len(str(n)))
