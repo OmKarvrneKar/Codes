e2a0c9922bcf0fb37135d59751522617016d0c6f
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-09 23:41:16 +0530

    Update bfs level traversal

--- bfs.py
+++ bfs.py
@@ -27,4 +27,21 @@ bfs(graph, 'A')
 
 
 
+def level(root):
+    if not root:
+        return []
+    result =[]
+    queue = deque([root])
+    while len(queue) >0:
+        level=[]
+        for i in range(len(queue)):
+            node =queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        result.append(level)
+    return result   
+
  
