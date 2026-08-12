4a5005fd9dd023d4e166b2c108e3c476b68c7d69
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-12 20:14:39 +0530

    Update workspace files

---
+++ Basic/dsa.py
@@ -0,0 +1,40 @@
+# for i in range(4):
+#     for j in range(4):
+#         print("*", end="")
+#     print()
+
+# for i in range(1,6):
+#     print("*" * i)
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j, end="")
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j, end="")
+#     print()
+
+
+#==============================
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end="")
+    print()
+
+for i in range(1,6):
+    for j in range(1,i+1):
+
+        print("*", end="")
+    print()
+
+for i in range(1,6):
+    for j in range(i,6):
+        print(i, end="")
+    print()
