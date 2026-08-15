f80b5215c69aa3afacf49c2c8b94cb6a667f5d6d
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-15 18:53:03 +0530

    feat: add nested loop implementations for printing numeric patterns to loops1.py

--- Basic/loops1.py
+++ Basic/loops1.py
@@ -9,8 +9,19 @@
 #         print("*", end="")
 #     print()
 # #==============
+
 for i in range(1,6):
-    for j in range(i+1,6):
+    for j in range(6-i,0,-1):
         print(j, end="")
     print()
-    
+#==============
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i, end="")
+    print()
+#=============`
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
+
