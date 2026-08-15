77fdfe2d425644cd0923540412915faf90e899ba
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-15 19:39:14 +0530

    feat: add loop exercises implementing various pattern printing algorithms

--- Basic/loops1.py
+++ Basic/loops1.py
@@ -26,4 +26,9 @@ for i in range(1,6):
     for j in range(1,i):
         print(j, end="")
     print()
+#=============
+for i in range(1,6):
+    for j in range(1,6-i+1):
 
+        print("*", end="")
+    print()
