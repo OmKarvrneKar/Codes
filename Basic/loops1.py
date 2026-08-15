03c9bb49684f63e6c1923e6e1a0e7b1bc80e66cd
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-15 19:23:54 +0530

    feat: add loops1.py with various nested loop pattern implementations

--- Basic/loops1.py
+++ Basic/loops1.py
@@ -21,7 +21,9 @@ for i in range(1,6):
     print()
 #=============`
 for i in range(1,6):
-    for j in range(1,i+1):
+    
+    print(i, end="")
+    for j in range(1,i):
         print(j, end="")
     print()
 
