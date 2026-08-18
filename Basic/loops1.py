5075ca8118fc4f96f3b73e74f7d74d0411eaff98
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-18 19:26:46 +0530

    feat: add nested loop pattern exercises to loops1.py

--- Basic/loops1.py
+++ Basic/loops1.py
@@ -51,8 +51,13 @@ for i in range(1,6):
         print(j, end="")
     print()
 #--------------------
+# 12345
+# 1234
+# 123
+# 12
+# 1
 for i in range(1,6):
-    for j in range(i+6,0,1):
+    for j in range(6-i,0,-2):
         print(j, end="")
     print()
-    6-i,0,-1
+    
