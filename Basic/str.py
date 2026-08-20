e74f95b369f7d914ab6973820e3a1326d6d54a61
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-20 19:44:31 +0530

    feat: add script to print placeholder based on vowel presence in input string

--- Basic/str.py
+++ Basic/str.py
@@ -3,7 +3,8 @@ n=input("enter the value")
 l=len(n)
 for i in range(l):
     if n[i] in "aeiouAEIOU":
-        q=n[i]
-        print(len(q), end=" ")
+        q=len(n[i])
+
+        print(q, end=" ")
 
 
