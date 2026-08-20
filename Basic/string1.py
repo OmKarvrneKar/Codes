9006e3e09c8fe468381ad78974cd0704cebb7984
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-20 20:05:16 +0530

    feat: add scriptin  str

--- Basic/string1.py
+++ Basic/string1.py
@@ -14,3 +14,14 @@ for i in n:
 print(len(m))
 
 #==================
+n=input("Enter the string: ")
+n.strip()
+m=[]
+q=[]
+for i in n:
+    if i in "AEIOUaeiou":
+        m.append(i)
+    else:
+        q.append(i)
+print("vowels",len(m))
+print("consonants",len(q)) 
