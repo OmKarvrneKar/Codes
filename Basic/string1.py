6f40714b19abecc2eaeebf694930e64bb856bc86
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-24 23:06:15 +0530

    feat: add new utility script for string operations

--- Basic/string1.py
+++ Basic/string1.py
@@ -62,3 +62,16 @@ text='I love Python programming'
 m=text.split()
 
 print(len(m)) 
+#==========
+text='aabbcdde'
+freq={}
+for i in text:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+for i in freq:
+    if freq[i]==1:
+        print(i)
+        break
+
