127e54dee464aefad5aa0744bb71564d3dfe0757
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-08-24 17:37:08 +0530

    feat: add new utility script for string operations

--- Basic/str.py
+++ Basic/str.py
@@ -1,3 +1,8 @@
-data=("  i love python  ")
-m=data.split(" ")
-print(m)
+text='aabbcdde'
+data=len(text)
+for i in range(data):
+    for j in range(i+1,data):
+        if text[i]==text[j]:
+            break
+    else:
+        print(text[i])
